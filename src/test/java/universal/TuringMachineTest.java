33598d5ade9bd8c158bb96eeeb1aa8243cf99ebd
Peter Kofler <peter.kofler@code-cop.org>
2024-01-30 21:51:11 +0100

    Format minimal test.

--- src/test/java/universal/TuringMachineTest.java
+++ src/test/java/universal/TuringMachineTest.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import universal.state.State;
-import universal.state.TransitionLookup;
 import universal.state.TransitionTable;
 import universal.tape.Direction;
 import universal.tape.Symbol;
@@ -17,45 +16,50 @@ import universal.tape.Tape;
 class TuringMachineTest {
 
     enum Alphabet implements Symbol {
-        Zero, //
-        One, //
-        Blank // end of tape
+        ZERO, //
+        ONE, //
+        END_OF_TAPE // 
     }
 
     enum Q implements State {
-        Zero {
-            @Override
-            public boolean isTerminal() {
-                return false;
-            }
-        },
-        Halt {
-            @Override
-            public boolean isTerminal() {
-                return true;
-            }
-        };
+        ZERO(false), //
+        HALT(true);
+
+        private boolean terminal;
+
+        private Q(boolean terminal) {
+            this.terminal = terminal;
+        }
+
+        @Override
+        public boolean isTerminal() {
+            return this.terminal;
+        }
     }
 
     @Test
-    void machineReplaces0With1() {
-        Q initial = Q.Zero;
-        List<Alphabet> _011010 = Arrays.asList(Alphabet.Zero, Alphabet.One, Alphabet.One, //
-                Alphabet.Zero, Alphabet.One, Alphabet.Zero, Alphabet.Blank);
-        Tape<Alphabet> tape = new Tape<>(_011010, Alphabet.Blank);
+    void replaces0With1() {
+        Q initialState = Q.ZERO;
+        List<Alphabet> _011010 = Arrays.asList( //
+                Alphabet.ZERO, Alphabet.ONE, Alphabet.ONE, //
+                Alphabet.ZERO, Alphabet.ONE, Alphabet.ZERO, //
+                Alphabet.END_OF_TAPE);
+        Tape<Alphabet> tape = new Tape<>(_011010, Alphabet.END_OF_TAPE);
 
-        TransitionLookup transitionLookup = new TransitionTable(). //
-                row(Q.Zero, Alphabet.Zero, null, Alphabet.One, Direction.RIGHT). //
-                row(Q.Zero, Alphabet.One, null, null, Direction.RIGHT). //
-                row(Q.Zero, Alphabet.Blank, Q.Halt, null, Direction.NONE); 
+        TransitionTable transitions = new TransitionTable(). //
+                row(Q.ZERO, Alphabet.ZERO, null, Alphabet.ONE, Direction.RIGHT). //
+                row(Q.ZERO, Alphabet.ONE, null, null, Direction.RIGHT). //
+                row(Q.ZERO, Alphabet.END_OF_TAPE, Q.HALT, null, Direction.NONE);
 
-        TuringMachine<Alphabet> machine = new TuringMachine<>(tape, transitionLookup, initial);
+        TuringMachine<Alphabet> machine = new TuringMachine<>(tape, transitions, initialState);
 
         machine.loop();
 
-        List<Alphabet> result = tape.getCells();
-        List<Alphabet> _111111 = Arrays.asList(Alphabet.One, Alphabet.One, Alphabet.One, //
-                Alphabet.One, Alphabet.One, Alphabet.One, Alphabet.Blank);
-        assertEquals(_111111, result);
+        List<Alphabet> expected_111111 = Arrays.asList( //
+                Alphabet.ONE, Alphabet.ONE, Alphabet.ONE, //
+                Alphabet.ONE, Alphabet.ONE, Alphabet.ONE, //
+                Alphabet.END_OF_TAPE);
+        List<Alphabet> actualResult = tape.getCells();
+        assertEquals(expected_111111, actualResult);
     }
 }
